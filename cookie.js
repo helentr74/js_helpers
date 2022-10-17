e759940cb7d0d6c0662acd7e83e0fd9bf4d985cf
Trapeznikova Elena <trapeznikova.elena.07.04@gmail.com>
2022-10-17 17:23:57 +0300

    cookie get/set functions

---
+++ cookie.js
@@ -0,0 +1,37 @@
+window.addEventListener('load', function () {
+
+    // возвращает куки с указанным name,
+    // или undefined, если ничего не найдено
+    function getCookie(name) {
+        let matches = document.cookie.match(new RegExp(
+            "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+        ));
+        return matches ? decodeURIComponent(matches[1]) : undefined;
+    }
+
+    function setCookie(name, value, options = {}) {
+        options = {
+            path: '/',
+            ...options
+        };
+
+        if (options.expires instanceof Date) {
+            options.expires = options.expires.toUTCString();
+        }
+
+        let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+
+        for (let optionKey in options) {
+            updatedCookie += "; " + optionKey;
+            let optionValue = options[optionKey];
+            if (optionValue !== true) {
+                updatedCookie += "=" + optionValue;
+            }
+        }
+
+        document.cookie = updatedCookie;
+    }
+
+    // Пример использования:
+    setCookie('user', 'John', {secure: true, 'max-age': 3600});
+})
